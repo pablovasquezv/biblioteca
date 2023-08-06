16ae29c7af30a2a96bac7217b4f614f2165ddedb
Pablov <juan.vasquez@valposystem.com>
2023-08-05 20:59:31 -0400

    Creación de ruta para formulario editar Autor

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 import ListAutorForm  from  './components/autor/ListAutorForm.vue';
 import AddAutorForm  from  './components/autor/AddAutorForm.vue';
+import EditAutorForm  from  './components/autor/EditAutorForm.vue';
 import AddEditorialForm  from  './components/editorial/AddEditorialForm.vue';
 import ListEditorialForm  from  './components/editorial/ListEditorialForm.vue';
 
@@ -10,6 +11,7 @@ Vue.use(VueRouter)
 const routes =[
     {path: '/listaAutores', component:ListAutorForm},
     {path: '/createAutores', component:AddAutorForm},
+    { path: '/editAutor/:id', name: 'editAutor', component: EditAutorForm},
     {path: '/createEditoriales', component:AddEditorialForm},
     {path: '/listaEditoriales', component:ListEditorialForm}
 ]
